859e8aac76ab3e13bab83807760b303f83fe9904
bradenmccoycg <119483557+bradenmccoycg@users.noreply.github.com>
2022-11-30 19:18:01 -0500

    Add files via upload

---
+++ Order.java
@@ -0,0 +1,69 @@
+public class Order {
+    static String pizzaSize;
+    static String pizzaTopping1;
+     static String pizzaTopping2;
+     static String pizzaTopping3;
+
+   static String beverage1;
+
+    static String beverage2;
+    static String beverage3;
+
+
+
+    public String getPizzaSize() {
+        return pizzaSize;
+    }
+
+    public String getBeverage1() {
+        return beverage1;
+    }
+
+    public String getBeverage2() {
+        return beverage2;
+    }
+
+    public String getBeverage3() {
+        return beverage3;
+    }
+
+    public String getPizzaTopping1() {
+        return pizzaTopping1;
+    }
+
+    public String getPizzaTopping2() {
+        return pizzaTopping2;
+    }
+
+    public String getPizzaTopping3() {
+        return pizzaTopping3;
+    }
+
+    public void setBeverage1(String beverage1) {
+        this.beverage1 = beverage1;
+    }
+
+    public void setBeverage2(String beverage2) {
+        this.beverage2 = beverage2;
+    }
+
+    public void setBeverage3(String beverage3) {
+        this.beverage3 = beverage3;
+    }
+
+    public void setPizzaTopping3(String pizzaTopping3) {
+        this.pizzaTopping3 = pizzaTopping3;
+    }
+
+    public void setPizzaSize(String pizzaSize) {
+        this.pizzaSize = pizzaSize;
+    }
+
+    public void setPizzaTopping1(String pizzaTopping1) {
+        this.pizzaTopping1 = pizzaTopping1;
+    }
+
+    public void setPizzaTopping2(String pizzaTopping2) {
+        this.pizzaTopping2 = pizzaTopping2;
+    }
+}
